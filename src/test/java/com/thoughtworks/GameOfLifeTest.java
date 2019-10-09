4bd9b96925b0dc304143099c0e11915c47771262
pranalikisannamdas <pranalinamdas96@gmail.com>
2019-10-09 10:25:30 +0530

    Add. apply rules 1,0,1

--- src/test/java/com/thoughtworks/GameOfLifeTest.java
+++ src/test/java/com/thoughtworks/GameOfLifeTest.java
@@ -8,30 +8,41 @@ class GameOfLifeTest {
     @Test
     void givenWorldOfOneCell_whenEvolve_thenShouldCreateNewWorldWithLiveCell(){
         int[] world = {0};
-        int[] world1 = {0,1,0};
+        int[] newWorld = {0,1,0};
         Rule rule = new Rule(0,0,0);
-        GameOfLife gol = new GameOfLife(world,rule);
+        GameOfLife gameOfLife = new GameOfLife(world,rule);
 
-        Assertions.assertArrayEquals(world1, gol.evolve());
+        Assertions.assertArrayEquals(newWorld, gameOfLife.evolve());
     }
 
     @Test
     void givenWorldOfTwoCell_whenEvolve_thenShouldCreateNewWorld(){
         int[] world = {0,0};
-        int[] world1 = {0,1,1,0};
+        int[] newWorld = {0,1,1,0};
         Rule rule = new Rule(0,0,0);
-        GameOfLife gol = new GameOfLife(world,rule);
+        GameOfLife gameOfLife = new GameOfLife(world,rule);
 
-        Assertions.assertArrayEquals(world1, gol.evolve());
+        Assertions.assertArrayEquals(newWorld, gameOfLife.evolve());
     }
 
     @Test
     void givenWorldOfThreeCell_whenEvolve_thenShouldCreateNewWorld(){
         int[] world = {0,0,0};
-        int[] world1 = {0,1,1,1,0};
+        int[] newWorld = {0,1,1,1,0};
         Rule rule = new Rule(0,0,0);
-        GameOfLife gol = new GameOfLife(world,rule);
+        GameOfLife gameOfLife = new GameOfLife(world,rule);
 
-        Assertions.assertArrayEquals(world1, gol.evolve());
+        Assertions.assertArrayEquals(newWorld, gameOfLife.evolve());
+    }
+
+    @Test
+    void givenWorldOfFourCell_whenEvolve_thenShouldCreateNewWorld(){
+        int[] world = {0,0,0,0};
+        int[] newWorld = {0,0,0,0,0,0};
+
+        Rule rule = new Rule(0,1,0);
+        GameOfLife gameOfLife = new GameOfLife(world, rule);
+
+        Assertions.assertArrayEquals(newWorld, gameOfLife.evolve());
     }
 }
